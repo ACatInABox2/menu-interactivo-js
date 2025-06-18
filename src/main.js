d9783427be70f9b8789d7ebeda8fac366e5b1fa3
ACatInABox2 <santiagouwu471@gmail.com>
2025-06-17 20:32:59 -0500

    funcion sin terminar

--- src/main.js
+++ src/main.js
@@ -6,3 +6,19 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 }); // esto es parte para crear la interfaz para interactuar con el usuario en consola.
+
+// ahora si, a iniciar a definir las funciones principales del programa.
+
+function Iniciar() {
+    console.log('¿desea iniciar el programa?')
+    rl.question('1 = Si/Yes, 2 = No', (respuesta1) => {
+        const opcion = Number(respuesta1)
+        if (respuesta1 === 1) {
+            console.log("iniciando programa..");
+            // siguiente variable
+        } else {
+            rl.close("goodbye")
+        }
+    })
+}
+Iniciar()
