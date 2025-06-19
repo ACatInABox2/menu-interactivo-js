304128308509e8247f1ed54938d3735d04cc71ae
ACatInABox2 <santiagouwu471@gmail.com>
2025-06-19 18:00:14 -0500

    completo documentacion funcion Iniciar().

--- src/main.js
+++ src/main.js
@@ -9,16 +9,19 @@ const rl = readline.createInterface({
 
 // ahora si, a iniciar a definir las funciones principales del programa.
 
-function Iniciar() {
-    console.log('¿desea iniciar el programa?')
-    rl.question('1 = Si/Yes, 2 = No', (respuesta1) => {
-        const opcion = Number(respuesta1)
-        if (respuesta1 === 1) {
-            console.log("iniciando programa..");
-            // siguiente variable
+function Iniciar() { // funcion a ejecutar para iniciar el programa
+    limpiarPantalla() // limpia la pantalla antes de iniciar.
+    console.log('¿desea iniciar el programa?') // mensaje 
+    rl.question('1 = Si/Yes, 2 = No', async (respuesta1) => { // pregunta si iniciar el programa
+        const opcion = Number(respuesta1) // convertimos la respuesta en un numero y la guardamos en una constante llamada opcion
+        if (opcion === 1) { // se verifica si la respuesta es si o no:
+            await esperar(2000); // espera 2 segundos antes de dar el mensaje
+            console.log("iniciando programa.."); // si entonces da el mensaje de iniciando programa
+            
         } else {
-            rl.close("goodbye")
+            console.log("adios/goodbye") // mensaje de adios
+            rl.close() // cierra la interfaz
         }
     })
 }
-Iniciar()
+Iniciar() // PRUEBA
