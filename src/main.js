f5a23c4df55c52f004a097b83749970da1e09ec8
ACatInABox2 <santiagouwu471@gmail.com>
2025-06-19 19:22:02 -0500

    queda pendiente refactor de funciones como mostrarMenu() para recursividad con promesas, queda pendiente investigar. 50 minutos trabajado

--- src/main.js
+++ src/main.js
@@ -9,6 +9,65 @@ const rl = readline.createInterface({
 
 // ahora si, a iniciar a definir las funciones principales del programa.
 
+async function textoMenu(a) {
+    // mensajes constantes
+    const msg1 = "Este es nuestro menu:"
+    const msg2 = "1. Apagar programa"
+    const msg3 = "2. Juegar un juego"
+    const msg4 = "3. Ver fecha y hora"
+    const msg5 = "4. Ver datos del programa"
+    const msg3v2 = "2. Volver a jugar un juego"
+    const msg = "0. Volver a la pestaña anterior"
+    if (a === 1) {
+        await esperar(500);
+        console.log(msg1);
+        await esperar(100);
+        console.log(msg2);
+        await esperar(100);
+        console.log(msg3);
+        await esperar(100);
+        console.log(msg4);
+        await esperar(100);
+    console.log(msg5);
+    } else if (a === 2) {
+        await esperar(500);
+        console.log(msg1);
+        await esperar(100);
+        console.log(msg);
+        await esperar(100);
+        console.log(msg2);
+        await esperar(100);
+        console.log(msg3v2);
+        await esperar(100);
+        console.log(msg4);
+        await esperar(100);
+        console.log(msg5);
+    }
+}
+
+ async function mostrarMenu() {
+    textoMenu(1);
+    rl.question('¿A donde quiere ir?', async (respuesta) => {
+        switch (Number(respuesta)) {
+            case 1:
+                console.log("Apagando programa..")
+                await esperar(300)
+                console.log("Programa apagado con exito!")
+                rl.close()
+                break;
+            case 2: 
+                console.log("Iniciando juego..")
+                jugarJuego() // FUNCION PENDIENTE POR DEFINIR.
+                textoMenu(2);
+                rl.question('¿A donde quiere ir?', async (respuesta2) => {
+                    
+                })
+        }
+    })
+}
+
+
+//funcion: Iniciar(), nos servira para iniciar el programa.
 function Iniciar() { // funcion a ejecutar para iniciar el programa
     limpiarPantalla() // limpia la pantalla antes de iniciar.
     console.log('¿desea iniciar el programa?') // mensaje 
@@ -17,7 +76,7 @@ function Iniciar() { // funcion a ejecutar para iniciar el programa
         if (opcion === 1) { // se verifica si la respuesta es si o no:
             await esperar(2000); // espera 2 segundos antes de dar el mensaje
             console.log("iniciando programa.."); // si entonces da el mensaje de iniciando programa
-            
+            mostrarMenu()
         } else {
             console.log("adios/goodbye") // mensaje de adios
             rl.close() // cierra la interfaz
