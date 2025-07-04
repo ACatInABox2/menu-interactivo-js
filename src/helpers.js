50b0793de239c49c4266d2dadb3847d6c199d412
ACatInABox2 <santiagouwu471@gmail.com>
2025-07-03 19:56:22 -0500

    porfin avanzando de nuevo, declaracion de nuevas funciones en helpers y main.

--- src/helpers.js
+++ src/helpers.js
@@ -2,8 +2,34 @@ function limpiarPantalla() {
     console.clear() // limpia la consola para cuando el programa esta muy lleno.
 }
 
+function mostrarTextoMenu(a) {
+  // declaracion de mensajes constantes.
+    const msg1 = "Este es nuestro menu:"
+    const msg2 = "1. Apagar programa"
+    const msg3 = "2. Jugar un juego"
+    const msg4 = "3. Ver fecha y hora"
+    const msg5 = "4. Ver datos del programa"
+    const msg3v2 = "2. Volver a jugar un juego"
+    const msg = "0. Volver a la pestaña anterior"
+  // rectificacion de la ubicacion del usuario para evitar mensajes redundantes o sin sentido.
+    if (a === 1) { // estado natural en el que debe iniciar el usuario
+        console.log(msg1);
+        console.log(msg2);
+        console.log(msg3);
+        console.log(msg4);
+        console.log(msg5);
+    } else if (a === 2) { // estado en el que usuario eligio la opcion 2
+        console.log(msg1);
+        console.log(msg);
+        console.log(msg2);
+        console.log(msg3v2);
+        console.log(msg4);
+        console.log(msg5);
+    } 
+}
+
 function esperar(ms) { // dentro de los parentesis pondremos los milisegundos a esperar.
   return new Promise(resolve => setTimeout(resolve, ms)); // nos devuelve una promesa que se cumple luego del tiempo en milisegundos.
 }
 // exportamos las funciones.
-module.exports = {esperar, limpiarPantalla} 
+module.exports = {esperar, limpiarPantalla, mostrarTextoMenu} 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // declaracion de valores constantes
 const readline = require("readline");
-const { esperar, limpiarPantalla } = require("./helpers"); // importamos las funciones de src/helpers.
+const { esperar, limpiarPantalla, mostrarTextoMenu} = require("./helpers"); // importamos las funciones de src/helpers.
+const { error } = require("console");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,63 +10,19 @@ const rl = readline.createInterface({
 
 // ahora si, a iniciar a definir las funciones principales del programa.
 
-async function textoMenu(a) {
-    // mensajes constantes
-    const msg1 = "Este es nuestro menu:"
-    const msg2 = "1. Apagar programa"
-    const msg3 = "2. Juegar un juego"
-    const msg4 = "3. Ver fecha y hora"
-    const msg5 = "4. Ver datos del programa"
-    const msg3v2 = "2. Volver a jugar un juego"
-    const msg = "0. Volver a la pestaña anterior"
-    if (a === 1) {
-        await esperar(500);
-        console.log(msg1);
-        await esperar(100);
-        console.log(msg2);
-        await esperar(100);
-        console.log(msg3);
-        await esperar(100);
-        console.log(msg4);
-        await esperar(100);
-    console.log(msg5);
-    } else if (a === 2) {
-        await esperar(500);
-        console.log(msg1);
-        await esperar(100);
-        console.log(msg);
-        await esperar(100);
-        console.log(msg2);
-        await esperar(100);
-        console.log(msg3v2);
-        await esperar(100);
-        console.log(msg4);
-        await esperar(100);
-        console.log(msg5);
-    }
+function preguntar(mensaje) { // creamos la rl.question en forma de promesa para recursividad.
+  return new Promise((resolve) => {
+    rl.question(mensaje, (respuesta) => {
+      resolve(respuesta);
+    });
+  });
 }
 
- async function mostrarMenu() {
-    textoMenu(1);
-    rl.question('¿A donde quiere ir?', async (respuesta) => {
-        switch (Number(respuesta)) {
-            case 1:
-                console.log("Apagando programa..")
-                await esperar(300)
-                console.log("Programa apagado con exito!")
-                rl.close()
-                break;
-            case 2: 
-                console.log("Iniciando juego..")
-                jugarJuego() // FUNCION PENDIENTE POR DEFINIR.
-                textoMenu(2);
-                rl.question('¿A donde quiere ir?', async (respuesta2) => {
-                    
-                })
-        }
-    })
+async function ejecutar() {
+    limpiarPantalla();
+    mostrarTextoMenu(1);
+    
 }
-
 
 //funcion: Iniciar(), nos servira para iniciar el programa.
 function Iniciar() { // funcion a ejecutar para iniciar el programa
@@ -76,11 +33,18 @@ function Iniciar() { // funcion a ejecutar para iniciar el programa
         if (opcion === 1) { // se verifica si la respuesta es si o no:
             await esperar(2000); // espera 2 segundos antes de dar el mensaje
             console.log("iniciando programa.."); // si entonces da el mensaje de iniciando programa
-            mostrarMenu()
-        } else {
+            await esperar(2000) // por temas de comodidad y fina coqueteria
+            ejecutar()
+        } else if (opcion === 2) {
             console.log("adios/goodbye") // mensaje de adios
             rl.close() // cierra la interfaz
+        } else {
+          const error1 = "Input no admitido, use 1 o 2"
+          console.error(error1);
+          console.log("vuelvalo a intentar")
+          await esperar(1000);
+          Iniciar()
         }
     })
 }
-Iniciar() // PRUEBA
+ Iniciar()
