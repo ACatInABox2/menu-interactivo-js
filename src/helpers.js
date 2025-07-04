d65dbcb727d000a15f8627de92012f7bf52fa491
ACatInABox2 <santiagouwu471@gmail.com>
2025-07-04 17:08:14 -0500

    codigo con errores, pero en funcionamiento basico, se necesita un test completo, preparando modulizacion.

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,14 @@
 // declaracion de valores constantes
 const readline = require("readline");
 const { error } = require("console");
-
+const error1 = "Input no admitido, use 1 o 2"
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 }); // esto es parte para crear la interfaz para interactuar con el usuario en consola.
 
 function limpiarPantalla() {
-     console.clear() // limpia la consola para cuando el programa esta muy lleno.
+    return console.clear() // limpia la consola para cuando el programa esta muy lleno.
  }
 
 function mostrarTextoMenu() {
@@ -33,6 +33,113 @@ function preguntar(mensaje) { // creamos la rl.question en forma de promesa para
   });
 }
 
+async function error1Mostrar() {
+  console.error(error1);
+  console.error("vuelvalo a intentar")
+  await esperar(1000);
+}
+
+function mostrarTextoJuego() {
+  // definicion de valores constantes
+  const msg1 = "Este es nuestro menu de juegos:"
+  const msg2 = "1. Adivinar numero"
+  const msg3 = "2. Operaciones rapidas"
+  const msg4 = "3. juego en desarrollo"
+  // impresion de valores constantes.
+  console.log(msg1);
+  console.log(msg2);
+  console.log(msg3);
+  console.log(msg4);
+}
+
+async function iniciarJuego() {
+   const msg = "Iniciando juego.."
+      await esperar(200);
+      console.log(msg);
+      await esperar(400);
+}
+
+async function volverAJugar(a) {
+  const opcion = await preguntar("¿Volver a jugar? Si = 1, No = 2 R: ");
+    if (opcion === "1") {
+      await a();
+    } else if (opcion === "2") {
+      console.log("okay");
+      await volver();
+    } else {
+      error1Mostrar();
+      await volverAJugar(a());
+    }
+}
+
+
+function numeroAleatorio() {
+  const numero = Math.floor(Math.random() * 10) + 1
+  return numero
+}
+
+async function adivinarUnNumero() {
+  limpiarPantalla();
+  let numero = numeroAleatorio();
+  const opcion = await preguntar("¿Cual es el numero? R: ");
+  if (Number(opcion) === numero) {
+    console.log("U WIN!");
+    await volverAJugar(adivinarUnNumero);
+  } else {
+    console.log("Incorrecto!");
+    console.log(numero)
+    await volverAJugar(adivinarUnNumero);
+  }
+}
+
+async function operacionesRapidas(c, d) {
+  limpiarPantalla();
+  const num1 = numeroAleatorio(c);
+  const num2 = numeroAleatorio(d);
+  console.log(num1 + " + " + num2);
+  const opcion = await preguntar("¿cual es la respuesta? R: ");
+  if (Number(opcion) === num1 + num2) {
+    console.log("U WIN!");
+    volverAJugar(operacionesRapidas);
+  } else {
+    console.log("Incorrecto");
+    volverAJugar(operacionesRapidas)
+  }
+}
+
+async function jugarUnJuego() {
+  limpiarPantalla();
+  mostrarTextoJuego();
+  const opcion = await preguntar("Elige una opción: ");
+  switch (opcion) {
+    case "1":
+      await iniciarJuego();
+      await adivinarUnNumero();
+      await volver();
+      break;
+    case "2":
+      await iniciarJuego();
+      await operacionesRapidas(20, 15);
+      await volver()
+      break;
+    case "3":
+      iniciarJuego();
+      console.log("juego en desarrollo..");
+      await volver()
+      break;
+  }
+}
+
+function datosPrograma() {
+  const info = {
+    Autor: "Santiago Torres",
+    Usuario_Github: "ACatInABox2",
+    Link_Repositorio_Actual: "https://github.com/ACatInABox2/menu-interactivo-js",
+    Version: "v1.0"
+  }
+  console.log(info)
+}
+
 async function ejecutar() {
     limpiarPantalla();
     mostrarTextoMenu();
@@ -43,12 +150,13 @@ async function ejecutar() {
         await esperar(1500);
         console.log("cerrando programa..");
         await esperar(1500);
-        rl.close();
+        return rl.close();
         break;
       case "2":
         limpiarPantalla();
         await esperar(500)
         console.log("juego en desarrollo.."); // TEMPORAL
+        await jugarUnJuego()
         await esperar(1500);
         await volver();
         break;
@@ -62,18 +170,25 @@ async function ejecutar() {
       case "4":
         limpiarPantalla();
         await esperar(1000);
-        datosPrograma(); // falta por definir
+        datosPrograma(); 
         await volver();
         break;
+      default:
+        error1Mostrar();
+        await esperar(2500)
+        ejecutar();
     }
 }
 
 async function volver() {
-  const opcion = await preguntar("¿Quieres volver al menu principal? Sí = 1, No = 2");
+  const opcion = await preguntar("¿Quieres volver al menu principal? Sí = 1, No = 2 R: ");
         if (opcion === "1") {
-          ejecutar();
+          await ejecutar();
+        } else if (opcion === "2") {
+          rl.close();
         } else {
-          rl.close()
+          await error1Mostrar();
+          await volver();
         }
 }
 
@@ -81,4 +196,4 @@ function esperar(ms) { // dentro de los parentesis pondremos los milisegundos a 
   return new Promise(resolve => setTimeout(resolve, ms)); // nos devuelve una promesa que se cumple luego del tiempo en milisegundos.
 }
 // exportamos las funciones.
-module.exports = {esperar, mostrarTextoMenu, volver, preguntar, ejecutar, rl, limpiarPantalla} 
+module.exports = {esperar, mostrarTextoMenu, volver, preguntar, ejecutar, rl, limpiarPantalla, error1Mostrar} 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // declaracion de valores constantes
-const { esperar, mostrarTextoMenu, volver, preguntar, ejecutar, rl, limpiarPantalla} = require("./helpers"); // importamos las funciones de src/helpers.
+const { esperar, mostrarTextoMenu, volver, preguntar, ejecutar, rl, limpiarPantalla, error1Mostrar} = require("./helpers"); // importamos las funciones de src/helpers.
 const { error } = require("console");
 
 // ahora si, a iniciar a definir las funciones principales del programa.
@@ -8,22 +8,19 @@ const { error } = require("console");
 function Iniciar() { // funcion a ejecutar para iniciar el programa
     // limpiarPantalla() // limpia la pantalla antes de iniciar.
     console.log('¿desea iniciar el programa?') // mensaje 
-    rl.question('1 = Si/Yes, 2 = No', async (respuesta1) => { // pregunta si iniciar el programa
+    rl.question('1 = Si/Yes, 2 = No ', async (respuesta1) => { // pregunta si iniciar el programa
         const opcion = Number(respuesta1) // convertimos la respuesta en un numero y la guardamos en una constante llamada opcion
         if (opcion === 1) { // se verifica si la respuesta es si o no:
             await esperar(2000); // espera 2 segundos antes de dar el mensaje
             console.log("iniciando programa.."); // si entonces da el mensaje de iniciando programa
             await esperar(2000) // por temas de comodidad y fina coqueteria
-            ejecutar()
+            ejecutar();
         } else if (opcion === 2) {
-            console.log("adios/goodbye") // mensaje de adios
+            console.log("adios/goodbye"); // mensaje de adios
             rl.close() // cierra la interfaz
         } else {
-          const error1 = "Input no admitido, use 1 o 2"
-          console.error(error1);
-          console.log("vuelvalo a intentar")
-          await esperar(1000);
-          Iniciar()
+          error1Mostrar();
+          Iniciar();
         }
     })
 }
